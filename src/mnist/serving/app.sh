de778f8ef6898a7e8168084158145bc4b605ffd5
Tsai Li Ming <tsai.liming@gmail.com>
2020-09-14 00:34:30 +0800

    test

--- src/mnist/serving/app.sh
+++ src/mnist/serving/app.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+MODEL_NAME=mnistddpserving
+API_TYPE=GRPC
+SERVICE_TYPE=MODEL
+PERSISTENCE=0
+
 seldon-core-microservice $MODEL_NAME $API_TYPE --service-type $SERVICE_TYPE --persistence $PERSISTENCE
